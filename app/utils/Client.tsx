dab370a3dddfaf4cc32229080cc8d5e2917632d4
Marcel Hauri <marcel.hauri@gmail.com>
2021-10-09 22:08:20 +0200

    [task] get basePath from Env

--- app/utils/Client.tsx
+++ app/utils/Client.tsx
@@ -1,7 +1,6 @@
 import {ApiClient} from "@mhauri/tier-api-client"
 
 const client = new ApiClient();
-//client.basePath = `${process.env.BASE_API_URL}`;
-client.basePath = `https://tier.api.hauri.dev`;
+client.basePath = `${process.env.BASE_API_URL}`;
 
 export const Client: ApiClient = client;
